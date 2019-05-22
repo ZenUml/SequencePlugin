77508f603d05be587ddd67725c7de5f1183f96bc
PengXiao <eagle.xiao@gmail.com>
2019-05-22 13:10:35 +1000

    Added a test for ClassDescription

---
+++ src/test/java/org/intellij/sequencer/generator/ClassDescriptionTest.java
@@ -0,0 +1,20 @@
+package org.intellij.sequencer.generator;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class ClassDescriptionTest {
+
+    @Test
+    public void test() {
+        ClassDescription classDescription = new ClassDescription("com.zenuml.A", new ArrayList());
+
+        assertThat(classDescription.getClassName(), is("com.zenuml.A"));
+        assertThat(classDescription.getClassShortName(), is("A"));
+    }
+
+}
---
+++ src/test/java/org/intellij/sequencer/generator/MethodDescriptionTest.java
@@ -0,0 +1,13 @@
+package org.intellij.sequencer.generator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MethodDescriptionTest {
+
+    @Test
+    public void test() {
+        MethodDescription.createMethodDescription(null, null, "", "", null, null);
+    }
+}
