c6fd2f56728c2aeeab141b22574260d5a6ee9723
PengXiao <eagle.xiao@gmail.com>
2019-06-07 08:51:07 +1000

    Can load local folder.

--- src/test/java/com/zenuml/testFramework/fixture/TestFixture.java
+++ src/test/java/com/zenuml/testFramework/fixture/TestFixture.java
@@ -1,7 +1,10 @@
 package com.zenuml.testFramework.fixture;
 
+import com.intellij.JavaTestUtil;
+import com.intellij.codeInspection.emptyMethod.EmptyMethodInspection;
 import com.intellij.openapi.application.ex.PathManagerEx;
 import com.intellij.psi.PsiClass;
+import com.intellij.testFramework.InspectionTestCase;
 import com.intellij.testFramework.UsefulTestCase;
 import com.intellij.testFramework.builders.JavaModuleFixtureBuilder;
 import com.intellij.testFramework.fixtures.*;
@@ -9,33 +12,23 @@ import org.junit.Test;
 
 import java.nio.file.Paths;
 
-public class TestFixture extends UsefulTestCase {
+public class TestFixture extends InspectionTestCase {
 
     @Override
-    public void setUp() throws Exception {
-        super.setUp();
-
-        final TestFixtureBuilder<IdeaProjectTestFixture> projectBuilder = IdeaTestFixtureFactory.getFixtureFactory().createFixtureBuilder("ZenUML");
-
-        JavaCodeInsightTestFixture myFixture = JavaTestFixtureFactory.getFixtureFactory().createCodeInsightFixture(projectBuilder.getFixture());
-        try {
-            final String dataPath = Paths.get("./test/data/").toAbsolutePath().toString();
-            myFixture.setTestDataPath(dataPath);
-            final JavaModuleFixtureBuilder builder = projectBuilder.addModule(JavaModuleFixtureBuilder.class);
-            builder.setMockJdkLevel(JavaModuleFixtureBuilder.MockJdkLevel.jdk15);
-
-            myFixture.setUp();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        System.out.println(myFixture.getModule());
-        PsiClass psiClass = myFixture.addClass("package foo.bar; public class PublicClass { public void method1() {} }");
-
-        System.out.println(psiClass);
-        System.out.println(psiClass.getMethods().length);
+    protected String getTestDataPath() {
+        return Paths.get("src/test/data/").toAbsolutePath().toString() + "/inspection";
     }
 
-    @Test
-    public void test() {
+    private void doTest() {
+        doTest(false);
+    }
+
+    private void doTest(final boolean checkRange) {
+        final EmptyMethodInspection tool = new EmptyMethodInspection();
+        doTest("emptyMethod/" + getTestName(true), tool, checkRange);
+    }
+
+    public void testInAnonymous() {
+        doTest(true);
     }
 }
