2e961cd1aaf89f8127148c867ca588da00ecb4b7
PengXiao <eagle.xiao@gmail.com>
2019-06-06 22:54:50 +1000

    Can create a test fixture now. Get PsiClass and PsiMethod from the fixture.

--- src/test/java/com/zenuml/testFramework/fixture/TestFixture.java
+++ src/test/java/com/zenuml/testFramework/fixture/TestFixture.java
@@ -1,10 +1,13 @@
 package com.zenuml.testFramework.fixture;
 
 import com.intellij.openapi.application.ex.PathManagerEx;
+import com.intellij.psi.PsiClass;
 import com.intellij.testFramework.UsefulTestCase;
 import com.intellij.testFramework.builders.JavaModuleFixtureBuilder;
 import com.intellij.testFramework.fixtures.*;
 import org.junit.Test;
+
+import java.nio.file.Paths;
 
 public class TestFixture extends UsefulTestCase {
 
@@ -16,7 +19,7 @@ public class TestFixture extends UsefulTestCase {
 
         JavaCodeInsightTestFixture myFixture = JavaTestFixtureFactory.getFixtureFactory().createCodeInsightFixture(projectBuilder.getFixture());
         try {
-            final String dataPath = PathManagerEx.getTestDataPath() + "/codeInsight/externalAnnotations";
+            final String dataPath = Paths.get("./test/data/").toAbsolutePath().toString();
             myFixture.setTestDataPath(dataPath);
             final JavaModuleFixtureBuilder builder = projectBuilder.addModule(JavaModuleFixtureBuilder.class);
             builder.setMockJdkLevel(JavaModuleFixtureBuilder.MockJdkLevel.jdk15);
@@ -26,7 +29,10 @@ public class TestFixture extends UsefulTestCase {
             e.printStackTrace();
         }
         System.out.println(myFixture.getModule());
+        PsiClass psiClass = myFixture.addClass("package foo.bar; public class PublicClass { public void method1() {} }");
 
+        System.out.println(psiClass);
+        System.out.println(psiClass.getMethods().length);
     }
 
     @Test
